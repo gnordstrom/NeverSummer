1e76840e33a57d4f7788733f87b512af4d7a78f7
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2016-12-08 12:08:55 -0700

    Completed styling of entire proto-type-two page, figured out the radials, and downloaded all necessary images

--- src/app/snowboards/snowboards-board-proto-type-two/snowboards-board-proto-type-two.component.ts
+++ src/app/snowboards/snowboards-board-proto-type-two/snowboards-board-proto-type-two.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-snowboards-board-proto-type-two',
@@ -9,7 +11,66 @@ export class SnowboardsBoardProtoTypeTwoComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    // Your jQuery code goes here
+    function Shortcodes() {
+
+  //Progress bar animations	
+		$('.progress-bar li').each(function(i){		
+			$(this).appear(function(){			
+				var percent = $(this).find('span').attr('data-width');
+				var $endNum = parseInt($(this).find('span strong i').text());
+				var $that = $(this);			
+				$(this).find('span').animate({
+					'width' : percent + '%'
+				},1600, function(){
+				});			
+				$(this).find('span strong').animate({
+					'opacity' : 1
+				},1400);			
+				$(this).find('span strong i').countTo({
+					from: 0,
+					to: $endNum,
+					speed: 1200,
+					refreshInterval: 30,
+					onComplete: function(){}
+				});	 
+				if(percent == '100'){
+					$that.find('span strong').addClass('full');
+				}	
+			});
+		});	
+
+    // Radial Counters
+  if( jQuery('.radial-counter').length > 0 ){		
+			$(".knob").knob({
+				width: 190,
+				height: 190,
+				fgColor: '#009ddd',
+				bgColor: '#fff',
+				inputColor: '#fff',
+				dynamicDraw: true,
+				thickness: 0.66,
+				tickColorizeValues: true,
+				skin:'tron',
+				readOnly:true,
+			});	
+			$(".knob").appear(function(e){			
+				var $this = $(this);
+				var myVal = $this.attr("data-gal");	
+			   $({value: 0}).animate({value: myVal}, {
+				   duration: 2000,
+				   easing: 'swing',
+				   step: function () {
+					   $this.val(Math.ceil(this.value)).trigger('change');
+				   }
+			   })			
+			});	
+		}
+}
+
   }
 
 }
